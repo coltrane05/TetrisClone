8e56f6e749b4fb25df7f3f19aedc7d0fbfb55af5
coltrane05 <crapocolton@gmail.com>
2025-04-05 17:44:20 -0600

    First Commit

---
+++ cmake-build-debug/CMakeFiles/TetrisClone.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TetrisClone.dir/main.cpp.o"
+  "CMakeFiles/TetrisClone.dir/main.cpp.o.d"
+  "TetrisClone"
+  "TetrisClone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TetrisClone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
